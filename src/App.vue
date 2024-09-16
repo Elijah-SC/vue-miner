8ebcf6486071dc6843d898bca0733dd940c1e35f
Elijah SasserCollins <ElijahJSasserCollins@gmail.com>
2024-09-16 14:15:57 -0600

    init

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import Navbar from './components/Navbar.vue';
+import { AppState } from './AppState.js';
+
+</script>
+
+<template>
+
+  <body class="container bg-dark">
+
+  </body>
+</template>
+
+<style lang="scss">
+@import '@/assets/scss/main.scss';
+</style>
